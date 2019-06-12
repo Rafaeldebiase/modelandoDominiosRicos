bebe273535b565a44331ad2e6b1d9d734d4cfd8d
Rafael de Biase <rafael.biase@opentechgr.com.br>
2019-06-12 13:29:55 -0300

    Verifica corrupção do código

--- paymentContext.domain/entities/students.cs
+++ paymentContext.domain/entities/students.cs
@@ -1,14 +1,37 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PaymentContext.Domain.Entities
 {
     public class Students
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Document { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }        
-        public List<Subscription> Subscription { get; set; }
+
+        private IList<Subscription> _subscription;
+
+        public Students(string firstName, string lastName, string document, string email, string address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Document = document;
+            Email = email;
+            Address = address;
+            _subscription = new List<Subscription>();
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Document { get; private set; }
+        public string Email { get; private set; }
+        public string Address { get; private set; }        
+        public IReadOnlyCollection<Subscription> Subscription { get { return _subscription.ToArray(); }}
+
+        public void AddSubscription(Subscription subscription) {
+            foreach (var sub in Subscription)
+            {   
+                sub.Inactivate();
+            }
+
+            _subscription.Add(subscription);
+        }
     }
 }
---
+++ paymentContext.test/entities/students.test.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace paymentContext.test
+{
+    [TestClass]
+    public class StudentsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
